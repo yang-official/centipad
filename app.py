77b9c43910397620c6495a871734e9e5a996b13c
yang-zhengyuan <yang.official@gmail.com>
2019-06-08 12:55:42 -0700

    Added docusign integration

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from docusign import embedded_signing_ceremony
+import webbrowser
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/docusign")
+def docusign():
+    webbrowser.open_new_tab(embedded_signing_ceremony())
+    return 'See New Tab'
 
 if __name__ == '__main__':
 	app.run(debug=True)
