502e5784fc9d37e455455ddd1bafb4c04652115b
yang-zhengyuan <yang.official@gmail.com>
2019-06-08 17:30:53 -0700

    Updated footers

--- google_ml.py
+++ google_ml.py
@@ -20,3 +20,28 @@ def classify_text(text):
         print(u'=' * 20)
         print(u'{:<16}: {}'.format('name', category.name))
         print(u'{:<16}: {}'.format('confidence', category.confidence))
+
+def analyze_entities(text):
+    client = language.LanguageServiceClient()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    # Instantiates a plain text document.
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects entities in the document. You can also analyze HTML with:
+    #   document.type == enums.Document.Type.HTML
+    entities = client.analyze_entities(document).entities
+
+    for entity in entities:
+        entity_type = enums.Entity.Type(entity.type)
+        print('=' * 20)
+        print(u'{:<16}: {}'.format('name', entity.name))
+        print(u'{:<16}: {}'.format('type', entity_type.name))
+        print(u'{:<16}: {}'.format('salience', entity.salience))
+        print(u'{:<16}: {}'.format('wikipedia_url',
+            entity.metadata.get('wikipedia_url', '-')))
+        print(u'{:<16}: {}'.format('mid', entity.metadata.get('mid', '-')))
