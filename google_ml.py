8f9a3c507226a8c98fea45970319e5d8658fe138
yang-zhengyuan <yang.official@gmail.com>
2019-06-09 09:48:16 -0700

    fixed links

--- google_ml.py
+++ google_ml.py
@@ -2,6 +2,7 @@ import six
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+import config
 
 def classify_text(text):
     """Classifies content categories of the provided text."""
